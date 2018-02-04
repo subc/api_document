93b4bde7b21379a933e998472bd936b49399d042
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2018-02-04 19:06:35 +0900

    refactor; api毎に部分テンプレート化した

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 # haml
 gem 'haml-rails'
 gem 'erb2haml'
-
+gem 'hashie'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,12 +2,19 @@ class ApiController < ActionController::Base
   protect_from_forgery with: :exception
 
   def show
+    @apis = apis
+
     id = params[:id].to_sym
     if id == :index
       render :file => "/api/show.html"
     elsif id == :api
       render :file => "/api/api.html"
     end
+  end
+
+  def apis
+    hash_tanaka = { name: 'Tanaka', age: 23 }
+    Hashie::Mash.new(hash_tanaka)
   end
 
 end
